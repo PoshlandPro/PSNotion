1b250865791e93181d5d51b43a285c7263abb3ec
Damian Garbus <kontakt@damiangarbus.pl>
2023-07-22 08:32:46 +0200

    update

--- .github/workflows/publish.ps1
+++ .github/workflows/publish.ps1
@@ -1,4 +1,4 @@
 $ModulePath = (Get-Item "$PSScriptRoot/../../PSNotion/").FullName
-Install-Module Microsoft.PowerShell.SecretStore
-Install-Module Microsoft.PowerShell.SecretManagement
+Install-Module Microsoft.PowerShell.SecretStore -Confirm:$false
+Install-Module Microsoft.PowerShell.SecretManagement -Confirm:$false
 Publish-Module -Path $ModulePath -NuGetApiKey $env:NUGET_KEY
