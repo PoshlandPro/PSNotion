93dae156b390cea67d8db4d779649fb67f5e317f
Damian Garbus <damian.garbus@ivanti.com>
2024-04-13 00:52:14 +0200

    feat: Add support for filtering records in Get-NotionPage function
    Added the ability to filter records retrieved by the Get-NotionPage function using the new $Filter parameter. This allows for more precise retrieval of data from Notion. Updated the function to handle filtering and added necessary logic for processing filtered results.

--- PSNotion/public/Get-NotionPage.ps1
+++ PSNotion/public/Get-NotionPage.ps1
@@ -13,23 +13,38 @@ function Get-NotionPage{
         [Parameter()]
         [Parameter(ParameterSetName = 'ByDatabaseId')]
         [switch]
-        $All
+        $All,
+        [Parameter()]
+        [Parameter(ParameterSetName = 'ByDatabaseId')]
+        [hashtable]
+        $Filter
     )
 
 
     if (![string]::IsNullOrEmpty($DatabaseId)) {
-        $Body = @{}
-        $Response = Invoke-NotionRequest -UriEndpoint "/databases/$DatabaseId/query" -Method POST -Body ( $Body | ConvertTo-Json)
+        if ($Filter) {
+            $Body = @{
+                filter = $Filter
+            }
+        }
+        else {
+            $Body = @{}
+        }
+
+        $Response = Invoke-NotionRequest -UriEndpoint "/databases/$DatabaseId/query" -Method POST -Body ( $Body | ConvertTo-Json -Depth 100)
         $ResponseSummary = $Response
     
     
         If ($All -and $Response.has_more -eq $true) {
             
             do {
-                $Body = @{
-                    start_cursor = "$($Response.next_cursor)"
+                if ($Body.ContainsKey("start_cursor")) {
+                    $Body["start_cursor"] = $Response.next_cursor
                 }
-                $Response = Invoke-NotionRequest -UriEndpoint "/databases/$DatabaseId/query" -Method POST -Body ( $Body | ConvertTo-Json)
+                else {
+                    $Body.Add("start_cursor", $Response.next_cursor)
+                }
+                $Response = Invoke-NotionRequest -UriEndpoint "/databases/$DatabaseId/query" -Method POST -Body ( $Body | ConvertTo-Json -Depth 100)
                 $ResponseSummary.results += $Response.results
                 $ResponseSummary.has_more = $Response.has_more
                 $ResponseSummary.next_cursor = $Response.next_cursor
